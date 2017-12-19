6aedfe8058fbac493f431698a567a385b5a7a324
Tyler Carr <tyler.m.carr@gmail.com>
2017-12-19 12:05:58 -0500

    work in progress

--- bin/titanic1.py
+++ bin/titanic1.py
@@ -1,8 +1,37 @@
 #!/usr/bin/python3
 #First Titanic python program
 
-class Titanic:
-	def __init__(self):
-		print("Starting Titanic machine learning exercise")
+import tensorflow as tf
+import csv
 
-titanic = Titanic()
+class Titanic:
+	self.FEATURES = ["Pclass","Sex","Age","SibSp","Parch"]
+	self.LABEL = ["Survived"]
+
+	def __init__(self, csvFilename):
+		print("Starting Titanic machine learning exercise")
+		self.loadCsvData(csvFilename)
+
+	def loadCsvData(self, csvFilename):
+		self.columnDict={}
+		self.dataDict={}
+		with open(csvFilename) as cf:
+			dataReader = csv.reader(cf, delimiter=',', quotechar='"')
+			rowNum = 1
+			for row in dataReader:
+				colNum=0
+				for colval in row:
+					if rowNum == 1:
+						self.columnDict[colNum]=colval
+						self.dataDict[colval]=[]
+					else:
+						self.dataDict[self.columnDict[colNum]].append(colval)
+					colNum=colNum+1
+				rowNum=rowNum+1
+
+	def printCsvDict(self):
+		print(str(self.dataDict))
+
+
+titanic = Titanic("../data/train.csv")
+titanic.printCsvDict()
